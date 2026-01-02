a5d0fdb8deb6af62aff197a866eb5b2c0720ecfd
ganesh pansare <grpansare2002@gmail.com>
2026-01-02 11:31:41 +0530

    initialize kanban service

---
+++ src/main/java/com/taskflow/kanban_service/KanbanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taskflow.kanban_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanServiceApplication.class, args);
+	}
+
+}
